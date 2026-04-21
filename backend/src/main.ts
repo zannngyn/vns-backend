4a675851d275b82a8ec5e7b112f34f8779366201
zannngyn <zanngyn@gmail.com>
2026-04-21 09:50:00 +0700

    feat: register Products and Cart modules and add BigInt serialization support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common';
 
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
